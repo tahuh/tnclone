f4ba9855eebc9b10049640f70faf7520abb4cb62
tahuh <tahuh1124@gmail.com>
2018-02-08 15:54:41 +0900

    Create wakeup.sh

---
+++ linux/wakeup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script is to give permission to user of using 
+# bwa, samtools, Trimmomatic, msa2vcf.jar
+# SSW-C, SSW-CPP
+
+chmod +x ./bwa
+chmod +x ./samtools
+chmod +x ./Trimmomatic-0.36/trimmomatic-0.36.jar
+chmod +x ./SSW-C
+chmod +x ./SSW-CPP
+chmod +x ./msa2vcf.jar
