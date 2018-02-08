dd41ebb01b5850c226060057f0eb4053a1aa688b
tahuh <tahuh1124@gmail.com>
2018-02-08 16:02:18 +0900

    Update wakeup.sh

--- linux/wakeup.sh
+++ linux/wakeup.sh
@@ -2,7 +2,7 @@
 
 # This script is to give permission to user of using 
 # bwa, samtools, Trimmomatic, msa2vcf.jar
-# SSW-C, SSW-CPP
+# SSW-C, SSW-CPP, mafft
 
 chmod +x ./bwa
 chmod +x ./samtools
@@ -10,3 +10,4 @@ chmod +x ./Trimmomatic-0.36/trimmomatic-0.36.jar
 chmod +x ./SSW-C
 chmod +x ./SSW-CPP
 chmod +x ./msa2vcf.jar
+chmod +x ./mafft
